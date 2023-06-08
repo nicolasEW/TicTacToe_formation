0e804724f2d0687cabbc08921553ab6101da84da
nicolas <nicolas@gmail.com>
2023-06-08 08:53:11 +0200

    function display finish

--- src/fr/nicolas/TicTacToe/Main.java
+++ src/fr/nicolas/TicTacToe/Main.java
@@ -7,5 +7,18 @@ import java.util.Scanner;
 public class Main {
 	public static String[][] board = { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " }, };
 
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+	}
+
+	/**
+	 * Affiche le tableau "board" dans la console
+	 */
+	public static void display() {
+		String[] letters = { "A", "B", "C" };
+		System.out.println("   1 2 3");
+		
+		for (int i = 0; i < board.length; i++) {
+			System.out.println(letters[i] + " |" + board[i][0] + "|" + board[i][1] + "|" + board[i][2] + "|");
+		}
+	}
 }
