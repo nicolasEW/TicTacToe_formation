ea9c8a529ba5ace617328201195682a87d20ecb0
nicolas <nicolas@gmail.com>
2023-06-08 08:50:54 +0200

    initialization of project

---
+++ src/fr/nicolas/TicTacToe/Main.java
@@ -0,0 +1,11 @@
+package fr.nicolas.TicTacToe;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+	public static String[][] board = { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " }, };
+
+	public static void main(String[] args) {}
+}
